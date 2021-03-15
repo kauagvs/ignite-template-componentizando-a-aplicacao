bc8e68cfc85bd7367fca72e884a8e2cf9910cbe4
Kauã <kaua.semenov@gmail.com>
2021-03-15 20:39:38 -0300

    Finished

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./../components/Button";
+
+import "../styles/sidebar.scss";
+
+import { GenreResponseProps } from "../App";
+
+interface SideBarProps {
+  genres: Array<GenreResponseProps>;
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar(props: SideBarProps) {
+  const { genres, selectedGenreId, handleClickButton } = props;
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
